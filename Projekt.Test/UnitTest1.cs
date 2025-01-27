62d7e1ea39bde414c20be238111fb7c5a8f9c03e
Bloczek227 <74356705+Bloczek227@users.noreply.github.com>
2025-01-28 00:06:25 +0100

    Add files via upload

--- Projekt.Test/UnitTest1.cs
+++ Projekt.Test/UnitTest1.cs
@@ -1,10 +1,12 @@
 ﻿
 
+using System.Globalization;
+
 namespace Projekt.Test
 {
     public class Tests
     {
-        Stats stats;
+        Stats stats=new();
 
         [TestCase(100, 200, false)]
         [TestCase(100, 80, true)]
@@ -13,13 +15,11 @@ namespace Projekt.Test
 
             Account acc = new(stats) { TickMoney = tickMoney };
 
-
             ClickUpgrade clickUpgrade = new("", acc, cost, 1.0);
             var result = clickUpgrade.Buy();
 
             Assert.That(result, Is.EqualTo(expectedResult));
         }
-
 
         [TestCase(100, 200, 100)]
         [TestCase(100, 80, 20)]
@@ -27,7 +27,6 @@ namespace Projekt.Test
         {
 
             Account acc = new(stats) { TickMoney = tickMoney };
-
 
             ClickUpgrade clickUpgrade = new("", acc, cost, 1.0);
             clickUpgrade.Buy();
@@ -44,7 +43,6 @@ namespace Projekt.Test
 
             Account acc = new(stats) { ClickMoney = clickMoney };
 
-
             TickUpgrade tickUpgrade = new("", acc, cost, 1.0);
             var result = tickUpgrade.Buy();
 
@@ -58,7 +56,6 @@ namespace Projekt.Test
         {
             Account acc = new(stats) { ClickMoney = clickMoney };
 
-
             TickUpgrade tickUpgrade = new("", acc, cost, 1.0);
             tickUpgrade.Buy();
             double result = acc.ClickMoney;
@@ -71,9 +68,7 @@ namespace Projekt.Test
         [TestCase(100, 80, 1)]
         public void TickBuyCountChange(double clickMoney, double cost, double expectedResult)
         {
-
             Account acc = new(stats) { ClickMoney = clickMoney };
-
 
             TickUpgrade tickUpgrade = new("", acc, cost, 1.0) { BoughtCount=0,Count=0};
             tickUpgrade.Buy();
@@ -88,9 +83,7 @@ namespace Projekt.Test
         [TestCase(100, 80, 1)]
         public void ClickBuyCountChange(double tickMoney, double cost, double expectedResult)
         {
-
             Account acc = new(stats) { TickMoney = tickMoney };
-
 
             ClickUpgrade clickUpgrade = new("", acc, cost, 1.0) { BoughtCount = 0, Count = 0 };
             clickUpgrade.Buy();
@@ -99,6 +92,26 @@ namespace Projekt.Test
 
             Assert.That(result, Is.EqualTo(expectedResult));
             Assert.That(result2, Is.EqualTo(expectedResult));
+        }
+
+
+
+        [TestCase(100, "100")]
+        [TestCase(123000, "123K")]
+        [TestCase(123450, "123.45K")]
+        [TestCase(123123123, "123.12M")]
+        [TestCase(12312312312, "12.31B")]
+        [TestCase(123123123123123123, "123.12Qa")]
+        [TestCase(1.23e35, "123D")]
+        [TestCase(1.2312e35, "123.12D")]
+        [TestCase(1.2312e36, "1.231E036")]
+        public void DoubleToIllionsConvert(double preConvert, string expectedResult)
+        {
+
+            NumberConverter numberConverter = new();
+            string result = (string)numberConverter.Convert(preConvert, typeof(string), null, new CultureInfo("en-US"));
+
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
